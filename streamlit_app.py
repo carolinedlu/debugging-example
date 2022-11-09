b636c589de7a331ed33ad4813f8758fbb74a4aa2
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2022-11-09 13:32:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import plost
+import pandas as pd
+
+datasets = [['Date','2022-09-30','2022/09/30','2022/09/30'],['sys',149,139,127],['dias',109,97,91]]
+df = pd.DataFrame(data, columns=['Date', 'sys', 'dias])
+df['Date']= pd.to_datetime(df['Date'])
 
 plost.bar_chart(
-    data=datasets,
+    data=df,
     bar='date',
     value=['sys', 'dias'],
     group=True)
